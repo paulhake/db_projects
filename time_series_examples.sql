c997ffabbfeff62bcb4b70ab498abd4f5ec6ddae
phake <phake@us.ibm.com>
2022-03-30 20:38:56 -0400

    0330

--- time_series_examples.sql
+++ time_series_examples.sql
@@ -1,15 +1,30 @@
 Time Series: ratio of womens to mens sales:note ratio calc and subQ to make code look cleaner
 
+#extract year from datetime column formated as text not date using STR_TO_DATE
+SELECT EXTRACT(YEAR from STR_TO_DATE(sales_month, '%Y-%m-%d')) AS year,SUM(sales)
+
+-- EXTRACT(YEAR_MONTH FROM STR_TO_DATE(Date, '%m/%d/%y'))
+
+FROM lessons.us_retail_sales
+WHERE kind_of_business = 'Retail and food services sales, total'
+GROUP BY 1;
+-- SELECT * FROM lessons.us_retail_sales
+
+#get column data types from table
+SELECT DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS
+WHERE table_name = 'citti_trans'
+
+
 SELECT sales_year
 ,(womens_sales / mens_sales - 1) * 100 as womens_pct_of_mens
 FROM
 (
     SELECT date_part('year',sales_month) as sales_year
-    ,sum(case when kind_of_business = 'Women''s clothing stores' 
-              then sales 
+    ,sum(case when kind_of_business = 'Women''s clothing stores'
+              then sales
               end) as womens_sales
-    ,sum(case when kind_of_business = 'Men''s clothing stores' 
-              then sales 
+    ,sum(case when kind_of_business = 'Men''s clothing stores'
+              then sales
               end) as mens_sales
     FROM retail_sales
     WHERE kind_of_business in ('Men''s clothing stores'
@@ -32,11 +47,11 @@ cast(replace('$19.99','$','')) as float
 #one hot
 Use Max to behave like one hot encoding with multiple	rows (max picks any )
 SELECT customer_id
-,max(case when fruit = 'apple' then 1 
-          else 0 
+,max(case when fruit = 'apple' then 1
+          else 0
           end) as bought_apples
-,max(case when fruit = 'orange' then 1 
-          else 0 
+,max(case when fruit = 'orange' then 1
+          else 0
           end) as bought_oranges
 FROM ...
 GROUP BY 1
